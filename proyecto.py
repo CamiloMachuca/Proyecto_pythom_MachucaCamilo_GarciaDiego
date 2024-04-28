de4e40070ffbc35cef57cc6cbd090a12d5139f0c
camilomachuca <camilomachuca03@gmail.com>
2024-04-28 10:23:48 -0500

    Proyecto

---
+++ proyecto.py
@@ -0,0 +1,24 @@
+
+
+# menu de opciones ya sea camper, Trainer o coordinador y cada uno va a tener un menu distinto de opciones disponibles
+
+
+# diseñar un programa para saber que camper paso de estar inscrito a aprobado 
+
+# Rutas disponibles: Ruta NodeJS, Ruta Java, Ruta NetCore capacidad maxima por ruta es de 33 campers
+
+# Hay clases cada 4 horas serian un total de 3 clases diarias
+
+# modulo de matricula Programa  para asignar la rutas de los campers esto lleva la Ruta, trainer encargado,fecha inicio y fecha final, salon de entrenamiento
+
+
+
+# crear trainers que trabajen en campuslands
+
+# Programa para saber la nota final de los campers ademas muestre los campers aprobados y los reprobados
+
+# programa para sacar el rendimiento de los campers en cada modulo y que permita consultar esos datos
+
+
+
+
