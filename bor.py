d312735f903a505dfe24f6dc462e976139d6dc04
camilomachuca <camilomachuca03@gmail.com>
2024-04-28 20:31:30 -0500

    Proyecto

--- bor.py
+++ bor.py
@@ -1,3 +1,10 @@
 c=input("ingresa la cantidad de notas: ")
+contador=0
+nota=[]
 for i in c:
-    i=input("ingresa nota: ")
+    n=input("ingresa nota: ")
+    contador=+1
+    nota=n
+
+    Nota_Final= (nota(i)*c)
+    print(Nota_Final)
